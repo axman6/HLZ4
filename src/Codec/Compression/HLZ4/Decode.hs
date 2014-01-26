83da7f1d9f1ece63728689107e9833fd28ed295c
Alex Mason <axman6@gmail.com>
2014-01-26 13:58:53 +1100

    Ported the PtrParser to use ContT .. StateT. Now has resume support
    Currently there are no functions to run the parser. Coming Soon™!

--- src/Codec/Compression/HLZ4/Decode.hs
+++ src/Codec/Compression/HLZ4/Decode.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE BangPatterns, ExistentialQuantification, 
+    TypeSynonymInstances, FlexibleInstances, MultiParamTypeClasses #-}
 
 module Codec.Compression.HLZ4.Decode
     -- (
@@ -6,68 +7,36 @@ module Codec.Compression.HLZ4.Decode
     -- ) 
     where
 
+
+import Control.Monad (when)
+import Control.Monad.Cont
+import Control.Monad.State.Strict
+
+import Data.Bits ((.|.), (.&.), shiftR, testBit, clearBit)
+
 import Data.ByteString (ByteString)
 import Data.ByteString.Internal (toForeignPtr)
-import Foreign.Storable (peekByteOff, peek, poke)
+
+import Data.Word (Word8, Word16, Word32)
+
+import Foreign.ForeignPtr (unsafeForeignPtrToPtr)
+
 import Foreign.Marshal.Alloc (mallocBytes)
 import Foreign.Marshal.Utils as F (copyBytes)
-import Foreign.Ptr (Ptr, plusPtr)
-import Data.Word (Word8, Word16, Word32)
-import Data.Bits ((.|.), (.&.), shiftR, testBit)
-import Control.Monad (when)
+import Foreign.Ptr (Ptr, plusPtr, nullPtr)
+import Foreign.Storable (peekByteOff, peek, poke)
+
+
+
+
+data HLZ4Result
+    = Done ByteString -- ^ Finished decoding all there is
+    | Block ByteString ByteString -- ^ Decoded a block, and remiaing input
+    | Partial (ByteString -> IO HLZ4Result) -- ^ More data is needed to decompress
+    | Error String -- ^ Something bad happened
+
 type P8 = Ptr Word8
 type PtrState = (P8, P8, Int, Int)
-
--- | A PtrParser is a monad which keeps track of the src and dst arrays
--- and how many bytes are left in both
-newtype PtrParser a = PtrParser (PtrState -> IO (Either String (PtrState, a)))
-
-runPtrParser :: PtrParser a -> PtrState -> IO (Either String (PtrState, a))
-runPtrParser (PtrParser f) s = f s
-
-
-
-
-instance Monad PtrParser where
-    return a = PtrParser $ \s -> return $ Right (s,a)
-    (PtrParser m) >>= f  = PtrParser $ \s -> do
-        x <- m s
-        case x of
-            Right (s', a) -> let PtrParser f' = f a in f' s'
-            Left str      -> return $ Left str
-    fail str = PtrParser $ \_ -> return $ Left str
-
-
-instance Functor PtrParser where
-    fmap f (PtrParser m) = PtrParser $ \s -> do
-        ex <- m s
-        return $! case ex of
-            Right (s',x) -> Right (s, f x)
-            Left str     -> Left str
-
-ensureSrc :: Int -> PtrParser ()
-ensureSrc n = PtrParser $ \s@(_,_,srem,_) -> 
-    return $ if n >= srem && srem >= 0 
-                then Right (s,())
-                else Left $ "ensureSrc: not enough bytes available in source. " ++ show (n, srem)
-
-
-ensureDest :: Int -> PtrParser ()
-ensureDest n = PtrParser $ \s@(_,_,_,drem) -> 
-    return $ if n >= drem && drem >= 0
-                then Right (s,()) 
-                else Left $ "ensureDest: not enough bytes available in destination. " ++ show (n,drem)
-
-ensureBoth :: Int -> PtrParser ()
-ensureBoth n = PtrParser $ \s@(_,_,srem,drem) -> 
-    return $ if n >= srem && srem >= 0
-                then if n >= drem && drem >= 0
-                        then Right (s,()) 
-                        else Left $ "ensureBoth: not enough bytes available in destination. " ++ show (n,drem)
-                else Left $ "ensureBoth: not enough bytes available in source. " ++ show (n, srem)
-
-atSrcEnd :: PtrParser Bool
-atSrcEnd = PtrParser $ \s@(_,_,srem,_) -> return $ Right (s,srem == 0)
 
 advanceSrcState :: Int -> PtrState -> PtrState
 advanceSrcState n (src,dst,srem,drem) =
@@ -83,92 +52,160 @@ advanceBothState n (src,dst,srem,drem) =
     (src `plusPtr` n, dst `plusPtr` n, srem - n, drem - n)
 
 
-advanceSrc :: Int -> PtrParser ()
-advanceSrc n = PtrParser $ \s ->
-    return $ Right (advanceSrcState n s,())
+type PtrParser a = ContT PPResult (StateT PtrState IO) a
 
-advanceDest :: Int -> PtrParser ()
-advanceDest n = PtrParser $ \s ->
-    return $ Right (advanceDestState n s,())
+-- runPP :: P8 -> Int -> PtrParser a -> IO HLZ4Result
+-- runPP src srem p = do
+--     let stT = runPtrParser p
+--     (res,(src',srem',dst',drem')) <- runStateT stT (src,nullPtr,srem,0)
+--     case res of
+--         PPPartial p' -> Partial (feedByteString dst' drem' p')
+-- 
+-- feedByteString :: P8 -> Int -> PtrParser a -> ByteString -> IO HLZ4Result
+-- feedByteString dst drem p bs = do
+--     let (srcptr, off, srem) = toForeignPtr bs
+--     runPP (unsafeForeignPtrToPtr srcptr `plusPtr` off) srem p
+-- 
+
+
+data PPResult
+    = PPDone P8 Int
+        -- ^ returns the src ptr and the length remaining, and a pointer to 
+    | PPDstPtr P8 Int (PtrParser PPResult) 
+        -- ^ when a new dest pointer is allocated, returns it and its length and the
+        -- parser to be run means that the parser doesn't have to keep track of
+        -- the beginning of the destination, runPtrParser can instead
+    | PPPartial (PtrParser PPResult)
+        -- ^ More input is required, pass in a Ptr Word8 and the length in bytes
+    | PPError String
+        -- ^ Something bad happened
+    -- | Value a -- not sure if needed
+
+
+err :: String -> PtrParser a
+err str = ContT $ \_ -> return $ PPError str
+
 
 
 getSrcRemaining :: PtrParser Int
-getSrcRemaining = PtrParser $ \s@(_,_,srem,_) -> return $ Right (s,srem)
+getSrcRemaining = get >>= \(_,_,srem,_) -> return srem
 
 getDestRemaining :: PtrParser Int
-getDestRemaining = PtrParser $ \s@(_,_,_,drem) -> return $ Right (s,drem)
+getDestRemaining = get >>= \(_,_,_,drem) -> return drem
+
+
+
+
+advanceSrc :: Int -> PtrParser ()
+advanceSrc n = modify (advanceSrcState n)
+
+advanceDest :: Int -> PtrParser ()
+advanceDest n = modify (advanceDestState n)
+
+advanceBoth :: Int -> PtrParser ()
+advanceBoth n = modify (advanceBothState n)
+
 
 peekByte :: Ptr Word8 -> Int -> IO Word8
 peekByte = peekByteOff
 
+demandInput :: PtrParser a -> PtrParser a
+demandInput p = ContT $ \next -> 
+    return $ PPPartial (p >>= \x -> ContT $ \c -> (next x >>= c))
+
 getWord8 :: PtrParser Word8
-getWord8 = PtrParser $ \s@(src,dst,srem,drem) -> do
-    b <- peek src
-    return $ Right (advanceSrcState 1 s,b)
+getWord8 = do
+    (src,_,srem,_) <- get
+    if srem >= 1
+        then do
+            b <- liftIO $ peekByte src 0
+            advanceSrc 1
+            return b
+        else demandInput getWord8 
 {-# INLINE getWord8 #-}
 
 getWord16LE :: PtrParser Word16
-getWord16LE = PtrParser $ \s@(src,_,_,_) -> do
-    a <- fromIntegral `fmap` peekByte src 0
-    b <- fromIntegral `fmap` peekByte src 1
-    return $ Right (advanceSrcState 2 s,(a `shiftR` 8 .|. b))
+getWord16LE = do
+    a' <- getWord8
+    b' <- getWord8
+    let [a,b] = map fromIntegral [a',b']
+    return (a `shiftR` 8 .|. b)
 {-# INLINE getWord16LE #-}
 
 
 getWord32LE :: PtrParser Word32
-getWord32LE = PtrParser $ \s@(src,_,_,_) -> do
-    a <- fromIntegral `fmap` peekByte src 0
-    b <- fromIntegral `fmap` peekByte src 1
-    c <- fromIntegral `fmap` peekByte src 2
-    d <- fromIntegral `fmap` peekByte src 3
-    return $ Right (advanceSrcState 4 s,(a `shiftR` 24 .|. b `shiftR` 16 .|. c `shiftR` 8 .|. d))
+getWord32LE = do
+    a' <- getWord8
+    b' <- getWord8
+    c' <- getWord8
+    d' <- getWord8
+    let [a,b,c,d] = map fromIntegral [a',b',c',d']
+    return (a `shiftR` 24 .|. b `shiftR` 16 .|. c `shiftR` 8 .|. d)
 {-# INLINE getWord32LE #-}
 
 
 setDestination :: Int -> PtrParser ()
-setDestination n = PtrParser $ \(src, _, srem, _) -> do
-    dst <- mallocBytes n
-    return $ Right ((src, dst, srem, n), ())
+setDestination n = do
+    dst <- liftIO $ mallocBytes n
+    (src, _, srem, _) <- get
+    put (src, dst, srem, n)
+    ContT $ \next -> do
+        let f :: (() -> StateT PtrState IO PPResult) -> ()
+            f _ = ()
+        return (f next)
+        return $ PPDstPtr dst n (ContT $ \c -> (next () >>= c))
 
 -- | Decodes a number encoded using repeated values of 255 and returns how many bytes were used
 getLength :: PtrParser Int
-getLength = PtrParser $ \s@(src,_,srem,_) -> do
-    let go !p 0  !_  = return $ Left "getLength: reached end of source."
-        go  p sr n   =  do
-            b <- peek p
-            case b :: Word8 of
-                255 -> go (p `plusPtr` 1) (sr-1) (n+255)
-                _ -> return $ Right (n + fromIntegral b, srem-(sr-1))
-    
-    r <- go src srem 0
-    return $! case r of
-        Right (n,len) -> Right (advanceSrcState len s, n)
-        Left str      -> Left str    
+getLength = go 0 where
+    go n = do
+        b <- getWord8
+        case b of
+            255 -> go (n+255)
+            _   -> return (n + fromIntegral b)   
 {-# INLINE getLength #-}
-
--- | Moves `count` bytes of data from `offset` bytes before current position into
--- the current position in the destination, and advances the state. Unchecked.
+ 
+-- | Transfers `count` bytes from src into dest. If there are not enough
+-- bytes in src, the remaining bytes will be copied and more input demanded.
+-- If there is not enough room in the destination and error is produced.
 transfer :: Int -> PtrParser ()
-transfer count = PtrParser $ \s@(src,dst,_,_) -> do
-    F.copyBytes dst src count
-    return $ Right (advanceBothState count s,())
+transfer count = do
+    (src,dst,srem,drem) <- get
+    case () of
+        _ | count <= srem && count <= drem -> do
+                liftIO $ F.copyBytes dst src count
+                advanceBoth count
+          | count > srem && count <= drem -> do
+                liftIO $ F.copyBytes dst src srem
+                advanceDest srem
+                demandInput (transfer (count-srem))
+          | otherwise -> err "transfer: requested too many bytes, destination overflow"
 
+-- 
 -- | Moves `count` bytes of data from `offset` bytes before current position into
 -- the current position in the destination, and advances the state. Unchecked.
 lookback :: Int -> Int -> PtrParser ()
-lookback count offset = PtrParser $ \s@(_,dst,_,_) -> do
+lookback count offset = do
+    (_,dst,_,drem) <- get
+    when (count > drem) $ err "lookback: requested too many bytes, destination overflow"
     let mmove :: Ptr Word8 -> Ptr Word8 -> Int -> IO ()
         mmove dest src 0 = return ()
         mmove dest src n = peek src >>= poke dest >> mmove (dest `plusPtr` 1) (src `plusPtr` 1) (n-1)
-    mmove dst (dst `plusPtr` (-offset)) count
-    return $ Right (advanceDestState count s,())
+    
+    liftIO $ mmove dst (dst `plusPtr` (-offset)) count
+    advanceDest count
 
-
+-- 
+-- 
 getSrcProgress :: Int -> PtrParser Int
-getSrcProgress n = PtrParser $ \s@(_,_,srem,_) -> return $ Right (s, n-srem)
+getSrcProgress n = do
+    srem <- getSrcRemaining
+    return $ n-srem
 
 getDestProgress :: Int -> PtrParser Int
-getDestProgress n = PtrParser $ \s@(_,_,_,drem) -> return $ Right (s, n-drem)
+getDestProgress n = do
+    drem <- getDestRemaining
+    return $ n-drem
 
 
 -- | Decodes a single LZ4 sequence within a block (lit len, lits, offset backwards, copy len).
@@ -176,7 +213,6 @@ getDestProgress n = PtrParser $ \s@(_,_,_,drem) -> return $ Right (s, n-drem)
 decodeSequence :: PtrParser Int
 decodeSequence = do
     sremBefore <- getSrcRemaining
-    ensureSrc 1
     token <- getWord8
     let lLen = fromIntegral $ (token .&. 0xF0) `shiftR` 4
         mLen = fromIntegral $ (token .&. 0x0F) + 4
@@ -184,19 +220,17 @@ decodeSequence = do
     litLength <- if lLen == 15 
                 then (15+) `fmap` getLength 
                 else return lLen
-    ensureBoth litLength
     transfer   litLength
     
     -- copy length from offset
     drem <- getDestRemaining
     when (drem > 0) $ do
-        ensureSrc 2
         offset <- getWord16LE
         matchLen <- if mLen == 19
             then (19+) `fmap` getLength
             else return mLen
-        ensureDest matchLen
         lookback matchLen (fromIntegral offset)
+    
     getSrcProgress sremBefore
 
 decodeSequences :: Int -> PtrParser ()
@@ -210,13 +244,11 @@ decodeSequences len
 getBlock :: PtrParser ()
 getBlock = do
     len' <- getWord32LE
-    let len = fromIntegral len'
-    setDestination (abs len)
-    if testBit len' 31 then do
-        ensureBoth (-len)
-        transfer (-len)
-    else
-        decodeSequences len
+    let len = fromIntegral (len' `clearBit` 31)
+    setDestination len
+    if testBit len' 31
+        then transfer len
+        else decodeSequences len
         
     
 
